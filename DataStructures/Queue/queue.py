187fb8fb208b045152c5ea5df9870e2e6d19be86
juanccancelado <j.canceladod@uniandes.edu.co>
2025-02-20 23:16:22 -0500

    cambios en funciones para hacer pruebas archivo excel

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,20 +1,21 @@
 from DataStructures.List import array_list as ar
+from DataStructures.List import single_linked_list as sl
 
 def new_queue():
-    new_queue = ar.new_list()
+    new_queue = sl.new_list()
     return new_queue
 
 def enqueue(my_queue, element):
-    return ar.add_last(my_queue, element)
+    return sl.add_last(my_queue, element)
 
 def dequeue(my_queue):
-    return ar.remove_first(my_queue)
+    return sl.remove_first(my_queue)
 
 def is_empty(my_queue):
-    return ar.is_empty(my_queue)
+    return sl.is_empty(my_queue)
     
 def peek(my_queue):
-    return ar.get_fist_element(my_queue)
+    return sl.first_element(my_queue)
 
 def size(my_queue):
-    return ar.size(my_queue)
+    return sl.size(my_queue)
